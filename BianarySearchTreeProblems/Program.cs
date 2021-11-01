20e37ed9e3cfbb35ed07c2aaf8524708c50cece5
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-01 17:06:17 +0530

    Adding Welcome message..

---
+++ BianarySearchTreeProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BianarySearchTreeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to bianary search tree...");
+        }
+    }
+}
